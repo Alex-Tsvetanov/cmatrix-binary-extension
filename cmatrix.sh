451d43a2a6aae4a1880b5fe70d1655c194a38f2a
Alex Tsvetanov <alex.tsvetanov.2002@gmail.com>
2017-08-06 02:47:00 +0300

    more options

--- cmatrix.sh
+++ cmatrix.sh
@@ -1,25 +1,26 @@
-echo -ne "\e[32m"; 
+symbols=$1
+if [[ -z "$symbols" ]];
+then
+	symbols="01"
+fi
+
+color=$2
+if [[ -z "$color" ]];
+then
+	color="32"
+fi
+
+
+echo -ne "\e[`echo -n $color`m"; 
 line=0
-COLUMNS=$((`tput cols` / 2));
+COLUMNS=$((`tput cols`));
 while $t; 
 do 
 	full_line=""; 
 	for i in `seq 1 $COLUMNS`; 
 	do 
-		r="$[($RANDOM % 2)]"; 
-		h="$[($RANDOM % 4)]"; 
-		if [ $h -eq 1 ]; 
-		then 
-			full_line="`echo -n $full_line`0$r"; 
-		else 
-			if [ $h -eq 2 ]; 
-			then 
-				full_line="`echo -n $full_line`1$r"; 
-			else
-				full_line="`echo -n $full_line` $r"; 
-			fi;
-		fi; 
-		#echo "asdf";
+		h=$(($RANDOM % ${#symbols})); 
+		full_line="`echo -n "$full_line"`${symbols:$h:1}"; 
 	done; 
 	echo $full_line;
 	if [[ $line -eq $((`tput lines`)) ]];
